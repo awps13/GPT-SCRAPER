f219a915546d6d9d2b9990b30d87a2f0b2b8ab7c
AHMAD WILDAN PUTRO SANTOSO <ahmadwildanputro13@gmail.com>
2025-03-13 09:05:03 +0700

    Update

--- src/app.py
+++ src/app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
