9cb4fed248d11083f9d42ab1a69b645b469b6d96
AHMAD WILDAN PUTRO SANTOSO <ahmadwildanputro13@gmail.com>
2025-03-17 08:58:10 +0700

    update

--- src/static/script.js
+++ src/static/script.js
@@ -1,26 +1,28 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-const showPassword1 = document.getElementById('togglePassword-sembunyi')
-const showPassword2 = document.getElementById('togglePassword-muncul')
-const pw = document.getElementById('password1')
+const container = document.getElementById("container");
+const registerBtn = document.getElementById("register");
+const loginBtn = document.getElementById("login");
+const showPassword1 = document.getElementById("togglePassword-sembunyi");
+const showPassword2 = document.getElementById("togglePassword-muncul");
+const pw = document.getElementById("password1");
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
+registerBtn.addEventListener("click", () => {
+  container.classList.add("active");
 });
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
+loginBtn.addEventListener("click", () => {
+  container.classList.remove("active");
 });
 
 function showPassword() {
-    showPassword1.setAttribute("hidden",true)
-    showPassword2.removeAttribute("hidden")
-    pw.removeAttribute("type")
+  showPassword1.setAttribute("hidden", true);
+  showPassword2.removeAttribute("hidden");
+  pw.removeAttribute("type");
 }
 
 function hidePassword() {
-    showPassword2.setAttribute("hidden",true)
-    showPassword1.removeAttribute("hidden")
-    pw.setAttribute("type","password")
+  showPassword2.setAttribute("hidden", true);
+  showPassword1.removeAttribute("hidden");
+  pw.setAttribute("type", "password");
 }
+
+
